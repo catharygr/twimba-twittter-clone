9271d8afc7137cfc065f987140942c28b87360bb
cathy <cathy@bubulazi.com>
2022-11-03 11:47:27 +0000

    Función generar html terminada

--- script/data.js
+++ script/data.js
@@ -1,7 +1,7 @@
 export const tweetsData = [   
   {
-      handle: `@claumanga 💎`,
-      profilePic: `images/clau.jpg`,
+      handle: `@claumanga ☣️`,
+      profilePic: `image/clau.jpg`,
       likes: 27,
       retweets: 10,
       tweetText: `Buy Bitcoin, ETH Make 💰💰💰 low low prices. 
@@ -12,20 +12,20 @@ export const tweetsData = [
       uuid: '4b161eee-c0f5-4545-9c4b-8562944223ee',
   },    
   {
-      handle: `@drkedrkedjon ✅`,
-      profilePic: `images/sasa.jpg`,
+      handle: `@drkedrkedjon 💩`,
+      profilePic: `image/sasa.jpg`,
       likes: 6500,
       retweets: 234,
       tweetText: `I need volunteers for a one-way mission to Mars 🪐. No experience necessary🚀`,
       replies: [
                 {
-              handle: `@zorkasajajevo ✅`,
-              profilePic: `images/zorka.jpg`,
+              handle: `@zorkasarajevo 😎`,
+              profilePic: `image/zorka.jpg`,
               tweetText: `Yes! Sign me up! 😎🛩`,
           },
                 {
-              handle: `@drkedrkedjon ✅`,
-              profilePic: `images/sasa.jpg`,
+              handle: `@drkedrkedjon 💩`,
+              profilePic: `image/sasa.jpg`,
               tweetText: `I went last year😴`,
           },
       ],
@@ -34,20 +34,20 @@ export const tweetsData = [
       uuid: '3c23454ee-c0f5-9g9g-9c4b-77835tgs2',
   },
       {
-      handle: `@zorkasarajevo`,
-      profilePic: `images/zorka.jpg`,
+      handle: `@zorkasarajevo 😎`,
+      profilePic: `image/zorka.jpg`,
       likes: 10,
       retweets: 3,
       tweetText: `Are you a coder if you only know HTML?`,
       replies: [
           {
               handle: `@claumanga ☣️`,
-              profilePic: `images/clau.jpg`,
+              profilePic: `image/clau.jpg`,
               tweetText: `No. Obviosuly not. Go get a job in McDonald's.`,
           },
           {
-              handle: `@drkedrkedjon`,
-              profilePic: `images/sasa.jpg`,
+              handle: `@drkedrkedjon 💩`,
+              profilePic: `image/sasa.jpg`,
               tweetText: `You are wonderful just as you are! ❤️`,
           },
       ],
--- script/index.js
+++ script/index.js
@@ -3,9 +3,51 @@ import {saveLocalStorage, readLocalStorage, getData} from "./utils.js"
 
 
 const twimbaFeed =  getData(tweetsData)
-console.log(twimbaFeed)
 
 
+function generarHtml() {
+  let html = ''
+  twimbaFeed.forEach(tweet => {
+    let replyHtml = ''
 
+    if(tweet.replies.length > 0) {
+      tweet.replies.forEach(reply => {
+        replyHtml += `
+        <div class="tweet-reply">
+        <div class="tweet">
+          <img src="${reply.profilePic}" alt="avatar" class="avatar">
+          <div class="tweet-contenido">
+            <p class="tweet-usuario">${reply.handle}</p>
+            <p>${reply.tweetText}</p>
+          </div>
+        </div>
+      </div>
+        `
+      })
+    }
+    html += `
+    <section class="tweet">
+    <img class="avatar" src="${tweet.profilePic}" alt="Avatar">
+    <div class="tweet-contenido">
+      <p class="tweet-usuario">${tweet.handle}</p>
+      <p>${tweet.tweetText}</p>
+      <div class="tweet-interacciones">
+        <span class="tweet-interaccion" ><i class="fa-regular fa-comment-dots"></i>2</span>
+        <span class="tweet-interaccion" ><i class="fa-regular fa-heart"></i>22</span>
+        <span class="tweet-interaccion" ><i class="fa-solid fa-retweet"></i>222</span>
+        <span class="tweet-interaccion" ><i class="fa-regular fa-reply"></i></span>
+      </div>
+      ${replyHtml}
+    </div>
+  </section>
+    `
+  });
+  return html
+}
 
+function renderHTML() {
+  document.querySelector('#tweet-feed').innerHTML = generarHtml()
+}
+
+renderHTML()
 
